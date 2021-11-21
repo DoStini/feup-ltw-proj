ca73d0ce24c51b6f2f0c2bd63b799bb55d8a568c
Nuno Alves <nyansword@gmail.com>
2021-11-21 17:03:02 +0000

    add: side bar responsiveness and script for opening and closing

--- client/script/script.js
+++ client/script/script.js
@@ -1,17 +1,16 @@
 function setupPopupWindows() {
-
     ["rules", "leaderboard"].forEach((target) => {
         document.getElementById(`${target}-open-btn`).addEventListener('click', () => {
             console.log("click" + target);
             let targetElement = document.getElementById(target);
 
-            if(targetElement.style.opacity === "1") {
+            if (targetElement.style.opacity === "1") {
                 targetElement.style.opacity = "0";
             } else {
                 targetElement.style.opacity = "1";
             }
 
-            if(targetElement.style.visibility === "visible") {
+            if (targetElement.style.visibility === "visible") {
                 targetElement.style.visibility = null;
             } else {
                 targetElement.style.visibility = "visible";
@@ -26,7 +25,7 @@ function setupPopupWindows() {
     })
 
     document.addEventListener('keydown', (e) => {
-        if(e.key === "Escape") {
+        if (e.key === "Escape") {
             console.log(document.getElementsByClassName("popup-window"))
             document.querySelectorAll(".popup-window").forEach((target) => {
                 target.style.opacity = null;
@@ -34,12 +33,30 @@ function setupPopupWindows() {
             })
         }
     });
+}
 
+function toggleGameStatus() {
+    toggleGameStatus.open = !toggleGameStatus.open || false;
+
+    if(toggleGameStatus.open === false) {
+        document.getElementById("game-status").style.right = (-document.getElementById("game-status-info").offsetWidth).toString() + "px";
+    } else {
+        document.getElementById("game-status").style.right = "0";
+    }
+
+}
+
+function setupGameStatus() {
+    let buttonElement = document.getElementById("game-status-button");
+
+    buttonElement.onclick = toggleGameStatus;
+    toggleGameStatus();
 }
 
 
 function main() {
     setupPopupWindows();
+    setupGameStatus();
 }
 
 main();
