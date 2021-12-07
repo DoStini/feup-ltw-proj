b3ae55d49717d521297a9a734f8149cc1899fa16
Nuno Alves <nyansword@gmail.com>
2021-12-07 09:26:48 +0000

    yeah fixed and added some things i think

--- client/script/mancala.js
+++ client/script/mancala.js
@@ -1,18 +1,160 @@
 "use strict";
 
-function Board(nHoles, nSeeds) {
-    this.seeds = new Array(2);
+function addMessage(messageText) {
+    const message = document.createElement("span");
+    const board = document.getElementById("message-board");
 
-    for (let i = 0; i < 2; i++) {
-        this.seeds[i] = new Array(nHoles);
+    message.innerText = messageText;
+    message.classList.add("message");
+    board.insertBefore(message, board.firstChild);
+}
 
-        for (let j = 0; j < nHoles; j++) {
-            this.seeds[i][j] = nSeeds;
+class Player {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class Seed {
+    constructor(id) {
+        this.id = id;
+        this.x = Math.random() * 60;
+        this.y = Math.random() * (70 - 8) + 8;
+        this.rot = Math.random() * 90;
+    }
+
+    render(parent) {
+        const newElem = document.createElement("div");
+        newElem.id = `seed${this.id}`;
+        newElem.className = "seed";
+        newElem.style.left = this.x + "%";
+        newElem.style.top = this.y + "%";
+        newElem.style.transform = `rotate(${this.rot}deg)`;
+
+        parent.appendChild(newElem);
+    }
+
+}
+
+class Board {
+    constructor(nHoles, nSeeds) {
+        this.storage = [[], []];
+        this.nHoles = nHoles;
+        this.nSeeds = nSeeds;
+        this.generateSeeds();
+    }
+
+    generateSeeds() {
+        const container = [];
+
+        for (let h = 0; h < this.nHoles * 2; h++) {
+            container.push([]);
+            for (let s = 0; s < this.nSeeds; s++) {
+                container[h][s] = new Seed(h*this.nSeeds + s);
+            }
+        }
+
+        this.seeds = container;
+    }
+
+    
+
+    render () {
+        const boardElement = document.getElementById("board");
+        let nHoles = this.nHoles;
+    
+        document.querySelectorAll("div.seed-box")
+            .forEach(el => {
+                el.style['grid-template-columns'] = `repeat(${this.nHoles}, 1fr)`;
+            });
+
+        boardElement.style['grid-template-columns'] = `repeat(${this.nHoles}, 1fr)`;
+
+        document.getElementById("board").innerHTML = null;
+
+        [1,0].forEach(el => {
+            const seedCounterWrapper = document.getElementById(`seeds${el}`);
+            seedCounterWrapper.innerHTML = "";
+
+            for (let i = 0; i < this.nHoles; i++) {
+                const idx = i + el * this.nHoles;
+                const seedCounter = document.createElement("span");
+                seedCounter.className = "seed-num";
+                seedCounter.id = `seeds${el}-${el === 0 ? i : this.nHoles - i}`;
+                console.log(idx, this.seeds)
+
+                seedCounter.innerHTML = this.seeds[idx].length;
+                seedCounterWrapper.appendChild(seedCounter);
+
+                const seedHole = document.createElement("div");
+                seedHole.className = `hole${el === 0 ? " player-hole" : ""}`;
+                console.log(seedHole)
+                seedHole.id = `hole${el}-${el === 0 ? i : this.nHoles - i}`;
+                boardElement.appendChild(seedHole);
+                this.seeds[idx].forEach(seed => {
+                    seed.render(seedHole);
+                });
+            }
+        });
+    }
+
+    selectSeeds(player, hole) {
+
+    }
+}
+
+class GameState {
+    /**
+    * 
+    * @param {Game} game
+    * @param {Board} board 
+    * @param {Player} player
+    */
+    constructor(game, board, player) {
+        this.board = board;
+        this.game = game;
+        this.player = player;
+    }
+    /**
+     *
+     * @param {Game} game
+     * @param {number} hole
+     */
+    sowSeeds(hole) { console.log("nothing to do in " + hole); }
+    run() { }
+    nextState() { };
+}
+
+
+class PlayerState extends GameState {
+    constructor(game, board, player) {
+        super(game, board, player);
+    }
+
+    /**
+     *
+     * @param {number} hole
+     */
+    sowSeeds(hole) {
+        console.log(hole);
+
+        this.nextState();
+    }
+    
+    run() {
+        document.getElementById(`name${this.player.id}`).classList.add("player-turn");
+
+        addMessage(MESSAGE.otherTurn(this.player.name));
+
+        for (let i = 0; i < this.board.nHoles; i++) {
+            document.getElementById(`hole0-${i}`).classList.add("player-hole");
         }
     }
 
-    this.storage = [0, 0];
-    this.nHoles = nHoles;
+    nextState() {
+        return new WaitState(this.game, this.board, this.player);
+    }
 }
 
 /**
@@ -20,56 +162,41 @@ function Board(nHoles, nSeeds) {
  * @param {Game} game
  * @param {Board} board 
  */
-function GameState(game, board) {
-    this.board = board;
-    this.game = game;
-}
-
-/**
- * 
- * @param {Game} game
- * @param {number} hole 
- */
-GameState.prototype.sowSeeds = function(hole) {console.log("nothing to do in " + hole)}
-
-GameState.prototype.run = function() {}
-
-
-function PlayerState(game, board) {
-    GameState.call(this, game, board);
-}
-
-PlayerState.prototype = Object.create(GameState.prototype);
-Object.defineProperty(PlayerState.prototype, 'constructor', {
-    value: PlayerState,
-    enumerable: false,
-    writable: true
-})
-
-/**
- * 
- * @param {number} hole 
- */
-PlayerState.prototype.sowSeeds = function (hole) {
-    console.log(hole);
-
-    this.game.changeState(new WaitState(this.game, this.board));
-}
-
-PlayerState.prototype.run = function() {
-    for(let i = 0; i < this.board.nHoles; i++) {
-        document.getElementById(`hole0-${i}`).classList.add("player-hole");
+class WaitState extends GameState {
+    constructor(game, board, player) {
+        super(game, board, player);
     }
 
-    const message = document.createElement("span");
-    const board =  document.getElementById("message-board");
+    run() {
+        document.getElementById(`name${this.player.id}`).classList.remove("player-turn");
 
-    document.getElementById("name0").classList.add("player-turn");
-    document.getElementById("name1").classList.remove("player-turn");
+        for (let i = 0; i < this.board.nHoles; i++) {
+            document.getElementById(`hole0-${i}`).classList.remove("player-hole");
+        }
+    }
 
-    message.innerText = "Player, it's your turn to play";
-    message.classList.add("message");
-    board.insertBefore(message, board.firstChild);
+    nextState() {
+        return new PlayerState(this.game, this.board, this.player);
+    }
+}
+
+class PlayAIState extends GameState {
+    constructor(game, board, player) {
+        super(game, board, player);
+    }
+
+    run() {
+        addMessage(MESSAGE.otherTurn(this.player.name));
+
+        setTimeout(function () {
+            this.game.nextPlayerState();
+            this.nextState();
+        }.bind(this), 2000);
+    }
+
+    nextState() {
+        return new WaitAIState(this.game, this.board, this.player);
+    }
 }
 
 /**
@@ -77,56 +204,54 @@ PlayerState.prototype.run = function() {
  * @param {Game} game
  * @param {Board} board 
  */
-function WaitState(game, board) {
-    GameState.call(this, game, board);
-}
-
-WaitState.prototype = Object.create(GameState.prototype);
-Object.defineProperty(WaitState.prototype, 'constructor', {
-    value: WaitState,
-    enumerable: false,
-    writable: true
-})
-
-WaitState.prototype.run = function() {
-    for(let i = 0; i < this.board.nHoles; i++) {
-        document.getElementById(`hole0-${i}`).classList.remove("player-hole");
+ class WaitAIState extends GameState {
+    constructor(game, board, player) {
+        super(game, board, player);
     }
 
-    document.getElementById("name1").classList.add("player-turn");
-    document.getElementById("name0").classList.remove("player-turn");
+    run() {
+        document.getElementById(`name${this.player.id}`).classList.remove("player-turn");
+    }
 
-    const message = document.createElement("span");
-    const board =  document.getElementById("message-board");
-
-    message.innerText = "It's the AI's turn";
-    message.classList.add("message");
-    board.insertBefore(message, board.firstChild);
-
-    setTimeout(function() {
-        this.game.changeState(new PlayerState(this.game, this.board));
-    }.bind(this), 2000);
+    nextState() {
+        return new PlayAIState(this.game, this.board, this.player);
+    }
 }
 
 /**
  * 
  * @param {GameState} state 
  */
-function Game() {
-    this.state;
-}
+class Game {
+    constructor() {
+        this.playerState;
+        this.enemyState;
+    }
+    /**
+     *
+     * @param {GameState} state
+     */
 
-/**
- * 
- * @param {GameState} state
- */
-Game.prototype.changeState = function(state) {
-    this.state = state;
-    this.state.run();
-}
+    nextPlayerState() {
+        this.changePlayerState(this.playerState.nextState());
+    }
+    nextEnemyState() {
+        this.changeEnemyState(this.enemyState.nextState());
+    }
 
-Game.prototype.sowSeeds = function(hole) {
-    this.state.sowSeeds(hole);
+    changePlayerState(state) {
+        this.playerState = state;
+        this.playerState.run();
+    }
+
+    changeEnemyState(state) {
+        this.enemyState = state;
+        this.enemyState.run();
+    }
+
+    sowSeeds(hole) {
+        this.playerState.sowSeeds(hole);
+    }
 }
 
 
@@ -135,34 +260,8 @@ Game.prototype.sowSeeds = function(hole) {
  * @param {Board} board  
  */
 function setupBoard(board) {
-    const boardElement = document.getElementById("board");
-    let nHoles = board.nHoles;
 
-    document.querySelectorAll("div.seed-box")
-        .forEach(el => {
-            el.style['grid-template-columns'] = `repeat(${nHoles}, 1fr)`;
-        })
-    boardElement.style['grid-template-columns'] = `repeat(${nHoles}, 1fr)`;
-
-    document.getElementById("board").innerHTML = null;
-
-    [1, 0].forEach(el => {
-        const seedCounterWrapper = document.getElementById(`seeds${el}`);
-        seedCounterWrapper.innerHTML = "";
-
-        for (let i = 0; i < nHoles; i++) {
-            const seedCounter = document.createElement("span");
-            seedCounter.className = "seed-num";
-            seedCounter.id = `seeds${el}-${i}`;
-            seedCounter.innerHTML = "0";
-            seedCounterWrapper.appendChild(seedCounter);
-
-            const seedHole = document.createElement("div");
-            seedHole.className = `hole ${el == 0 ? " player-hole" : ""}`;
-            seedHole.id = `hole${el}-${i}`;
-            boardElement.appendChild(seedHole);
-        }
-    });
+    board.render();
 }
 
 /**
@@ -170,28 +269,7 @@ function setupBoard(board) {
  * @param {Board} board  
  */
 function setupSeeds(board) {
-    let nHoles = board.nHoles;
-
-    [1, 0].forEach(el => {
-        for (let i = 0; i < nHoles; i++) {
-            const elem = document.getElementById(`hole${el}-${i}`);
-
-            for (let j = 0; j < board.seeds[el][i]; j++) {
-                const y = Math.random() * (70 - 8) + 8;
-                const x = Math.random() * 60;
-                const rot = Math.random() * 90;
-
-                const newElem = document.createElement("div");
-                newElem.id = `seed${el}-${i}-${j}`;
-                newElem.className = "seed";
-                newElem.style.left = x + "%";
-                newElem.style.top = y + "%";
-                newElem.style.transform = `rotate(${rot}deg)`;
-
-                elem.appendChild(newElem);
-            }
-        }
-    });
+    
 }
 
 /**
@@ -211,15 +289,20 @@ function setupGame(nHoles, seedsPerHole, turn) {
     setupBoard(board);
     setupSeeds(board);
 
-    let gameState;
+    let gameState, enemyState;
 
     const game = new Game();
-    if(turn === 0) {
-        gameState = new PlayerState(game, board);
+    const player1 = new Player(0, "Player 1");
+    const player2 = new Player(1, "AI");
+    if (turn === 0) {
+        gameState = new PlayerState(game, board, player1);
+        enemyState = new WaitAIState(game, board, player2);
     } else {
-        gameState = new WaitState(game, board);
+        gameState = new WaitState(game, board, player1);
+        enemyState = new PlayAIState(game, board, player2);
     }
-    game.changeState(gameState);
+    game.changePlayerState(gameState);
+    game.changeEnemyState(enemyState);
 
     setupHoles(game);
 }
--- client/script/script.js
+++ client/script/script.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 function setupPopupWindows() {
     ["rules", "leaderboard"].forEach((target) => {
