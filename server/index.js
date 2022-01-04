c52e7187a504d253a721b331fabd7494df8160fb
André Moreira <andre.moreira.9@hotmail.com>
2022-01-04 15:58:12 +0000

    Add server boilerplate

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
 const config = require("./src/env");
+const Framework = require("./framework/framework");
+const loaders = require("./src/loaders");
+
+const app = new Framework();
+
+loaders(app);
+
+app.listen(config.PORT);
