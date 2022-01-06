95aa306e0f94693b60a9b2df6bf0704a9fd568fa
Nuno Alves <nyansword@gmail.com>
2022-01-06 00:15:23 +0000

    change: removed useless things

--- server/src/loaders/index.js
+++ server/src/loaders/index.js
@@ -1,5 +1,4 @@
 const Framework = require("../../framework/framework");
-const UserHelper = require("../database/userController");
 const routes = require("../routes");
 const database = require("./database");
 
--- server/src/routes/ranking.js
+++ server/src/routes/ranking.js
@@ -7,8 +7,6 @@ const UserController = require('../database/userController');
  * @param {UserController} userController 
  */
  module.exports = (router, userController) => {
-    const databaseName = "auth";
-
     router.post("/ranking", async (request, response) => {
         return response.status(200).json(await userController.getRanking(10));
     });
