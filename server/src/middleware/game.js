cb4d13f8d52bad80648d2f481166120e3fc068bf
Nuno Alves <nyansword@gmail.com>
2022-01-07 19:24:21 +0000

    refactor: move validation error and test number to validation file

--- server/src/middleware/game.js
+++ server/src/middleware/game.js
@@ -1,6 +1,7 @@
 const Middleware = require("../../framework/middleware/middleware");
 const GameController = require("../services/gameController");
 const { fieldsValidator, requestError } = require("../utils");
+const { testNumber } = require("./validation");
 
 const join = fieldsValidator([
     "nick",
@@ -53,29 +54,6 @@ const userInGame = (location, gameController) => async (req, res, next) => {
     }
 
     next(req, res);
-}
-
-class ValidationError extends Error {
-    constructor(message, status) {
-        super(message);
-        this.status = status;
-    }
-}
-
-function testNumber(req, location, name, min, max) {
-    const val = parseInt(req[location][name]);
-
-    if(isNaN(val)) {
-        throw new ValidationError(`The ${name} must be a number`, 400);
-    }
-
-    if(min != null && val < min) {
-        throw new ValidationError(`The ${name} must be greater than or equal to ${min}`, 400);
-    }
-
-    if(max != null && val > max) {
-        throw new ValidationError(`The ${name} must be lesser than or equal to ${max}`, 400);
-    }
 }
 
 /**
