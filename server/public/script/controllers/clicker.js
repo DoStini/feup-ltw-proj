c1c9fb37d7324335c61c83c0b5eb96f340e28d9c
Nuno Alves <nyansword@gmail.com>
2022-01-13 13:14:16 +0000

    feat: add cookie hover and colors

--- server/public/script/controllers/clicker.js
+++ server/public/script/controllers/clicker.js
@@ -6,18 +6,41 @@ class Clicker {
     /** @type {Position} */
     center;
     frameHandle;
+    /** @type {Cookie} */
+    cookie;
 
     constructor() {
         this.canvas = document.getElementById("waiting-clicker");
+        this.canvas.onmousemove = this.handleMouseMove.bind(this);
         this.ctx = this.canvas.getContext("2d");
         this.center = new Position(this.canvas.width/2, this.canvas.height/2);
+        this.cookie = new Cookie(this.center, 50, "#84563c", "#bd8c61", 5);
         this.run();
+    }
+
+    mouseToCoords(event) {
+        let rect = this.canvas.getBoundingClientRect();
+
+        return new Position(event.clientX - rect.left, event.clientY - rect.top);
+    }
+
+    handleMouseMove(e) {
+        let position = this.mouseToCoords(e);
+
+        if(this.cookie.inside(position)) {
+            this.cookie.fillColor = "#9f7a59";
+            document.body.style.cursor = "pointer";
+        } else {
+            this.cookie.fillColor = "#bd8c61";
+            document.body.style.cursor = "default";
+        }
+
     }
 
     run() {
         this.ctx.beginPath();
-        this.ctx.arc(this.center.x, this.center.y, 50, 0, 2 * Math.PI);
-        this.ctx.stroke();
+
+        this.cookie.draw(this.ctx);
 
         this.frameHandle = requestAnimationFrame(this.run.bind(this));
     }
---
+++ server/public/script/models/cookie.js
@@ -0,0 +1,18 @@
+class Cookie extends Circle {
+    /** @type {Array.<Circle>} */
+    chips = [];
+    
+    constructor(position, radius, stroke, fill, strokeWidth) {
+        super(position, radius, stroke, fill, strokeWidth);
+    }
+
+    genChips(numChips, chipStroke, chipFill) {
+        for(let i = 0; i < numChips; i++) {
+
+        }
+    }
+
+    // draw(ctx) {
+        
+    // }
+}
