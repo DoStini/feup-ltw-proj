f9a4ec55952a02ae4599a5effe4bf289b091f45d
Nuno Alves <nyansword@gmail.com>
2022-01-13 16:50:47 +0000

    feat: hover animation

--- server/public/script/controllers/clicker.js
+++ server/public/script/controllers/clicker.js
@@ -18,26 +18,9 @@ class Clicker {
         this.ctx = this.canvas.getContext("2d");
         this.center = new Position(this.canvas.width/2, this.canvas.height/2);
         this.cookie = new Cookie(this.center, 50, "#84563c", "#bd8c61", 5);
-        this.animation = new CookieAnimation(60);
-        this.animation.addKeyFrame(new KeyFrame(((ctx) => this.cookie.draw(ctx)).bind(this), 0));
-        this.animation.addKeyFrame(new KeyFrame((ctx, frame) => {
-            ctx.save();
-
-            ctx.translate(this.center.x, this.center.y);
-            ctx.scale(1 + 0.01 * frame, 1 + 0.01 * frame);
-            ctx.translate(-this.center.x, -this.center.y);
-
-
-            this.cookie.draw(ctx);
-
-            if(this.hover) {
-                this.overlay.draw(this.ctx);
-            }
-            ctx.restore();
-        }, 0.5));
+        this.animation = new NoAnimation(this);
 
         this.animation.start();
-        this.overlay = new Circle(this.center, 50, "rgba(0,0,0,0)", "rgba(0,0,0,0.2)", 0);
         this.cookie.genChips(10, "#270d0b", "#5A2C22", 8, 3);
         this.hover = false;
         this.run();
@@ -54,8 +37,15 @@ class Clicker {
 
         if(this.cookie.inside(position)) {
             this.hover = true;
+            if(!(this.animation instanceof HoverAnimation)) {
+                this.animation = new HoverAnimation(10, 1.15, this);
+                this.animation.start();
+            }
+
             document.body.style.cursor = "pointer";
         } else {
+            this.animation = new NoAnimation(this);
+            this.animation.start();
             this.hover = false;
             document.body.style.cursor = "default";
         }
--- server/public/script/models/cookieAnimation.js
+++ server/public/script/models/cookieAnimation.js
@@ -21,9 +21,12 @@ class CookieAnimation {
     /** @type {number} */
     curKey;
     lastKeyFrame;
+    /** @type {boolean} */
+    repeats;
 
-    constructor(duration) {
+    constructor(duration, repeats) {
         this.duration = duration;
+        this.repeats = repeats;
     }
     
     addKeyFrame(keyframe) {
@@ -37,21 +40,18 @@ class CookieAnimation {
     }
 
     next(ctx) {
-        this.keyframes[this.curKey].draw(ctx, this.curFrame - this.lastKeyFrame);
-        console.log(this.curFrame / this.duration);
-
-        this.curFrame++;
-
-        if(this.end()) {
+        if(this.end() && this.repeats) {
             this.curFrame = 0;
             this.lastKeyFrame = 0;
             this.curKey = 0;
-        } else {
-            if(this.curKey < this.keyframes.length - 1 && this.curFrame / this.duration >= this.keyframes[this.curKey + 1].start) {
-                this.curKey++;
-                this.lastKeyFrame = this.curFrame;
-            }
+        } else if(this.curKey < this.keyframes.length - 1 && this.curFrame / this.duration >= this.keyframes[this.curKey + 1].start) {
+            this.curKey++;
+            this.lastKeyFrame = this.curFrame;
         }
+
+        this.keyframes[this.curKey].draw(ctx, this.curFrame - this.lastKeyFrame);
+
+        this.curFrame++;
     }
 
     end() {
@@ -59,10 +59,71 @@ class CookieAnimation {
     }
 }
 
+class NoAnimation extends CookieAnimation {
+    constructor(clicker) {
+        super(1, false);
+
+        this.addKeyFrame(new KeyFrame((ctx, frame) => {
+            clicker.cookie.draw(ctx);
+        }, 0))
+    }
+}
+
+class HoverAnimation extends CookieAnimation {
+    constructor(duration, endRadius, clicker) {
+        super(duration, false);
+        endRadius = endRadius - 1;
+        let middleRadius = endRadius / 1.25;
+        let increment = endRadius / (duration * 0.5);
+        let midIncrement = (endRadius - middleRadius) / (duration * 0.5);
+        this.overlay = new Circle(clicker.center, clicker.cookie.radius, "rgba(0,0,0,0)", "rgba(0,0,0,0.2)", 0);
+
+        this.addKeyFrame(new KeyFrame((ctx, frame) => {
+            ctx.save();
+
+            ctx.translate(clicker.center.x, clicker.center.y);
+            ctx.scale(1 + increment * frame, 1 + increment * frame);
+            ctx.translate(-clicker.center.x, -clicker.center.y);
+
+            clicker.cookie.draw(ctx);
+            this.overlay.draw(ctx);
+
+            ctx.restore();
+        },0))
+
+        this.addKeyFrame(new KeyFrame((ctx, frame) => {
+            ctx.save();
+
+            ctx.translate(clicker.center.x, clicker.center.y);
+            ctx.scale(1+ endRadius - midIncrement * frame, 1 + endRadius - midIncrement * frame);
+            ctx.translate(-clicker.center.x, -clicker.center.y);
+
+            clicker.cookie.draw(ctx);
+            this.overlay.draw(ctx);
+
+            ctx.restore();
+        },0.5))
+
+        this.addKeyFrame(new KeyFrame((ctx, frame) => {
+            ctx.save();
+            console.log("hey");
+
+            ctx.translate(clicker.center.x, clicker.center.y);
+            ctx.scale(1+ middleRadius, 1 + middleRadius);
+            ctx.translate(-clicker.center.x, -clicker.center.y);
+
+            clicker.cookie.draw(ctx);
+            this.overlay.draw(ctx);
+
+            ctx.restore();
+        },1))
+    }
+}
+
 /**
  * @callback KeyFrameHandler
  * @param {CanvasRenderingContext2D} ctx
- * @param {number} start
+ * @param {number} frame
  * 
  * @returns {void}
  */
